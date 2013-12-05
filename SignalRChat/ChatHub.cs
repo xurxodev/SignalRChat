1179cfa5ffb8d2285f3f3a1aa6fb8a03324cd9c1
xurxosf <xurxosf@gmail.com>
2013-12-05 22:15:17 +0100

    Añado ficheros del ejemplo

---
+++ SignalRChat/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat
+{
+    public class ChatHub : Hub
+    {
+        public void SendMessage(string userName, string message)
+        {
+            Clients.All.addMessage(userName, message);
+        }
+    }
+}
